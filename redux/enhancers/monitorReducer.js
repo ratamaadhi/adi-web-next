738d8feb75a51e1852fb60be9a8f3f53d47ae441
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2021-12-25 19:39:17 +0700

    fix(deploy):  comment performance.now()

--- redux/enhancers/monitorReducer.js
+++ redux/enhancers/monitorReducer.js
@@ -1,14 +1,14 @@
-const round = number => Math.round(number * 100) / 100
+// const round = number => Math.round(number * 100) / 100
 
 const monitorReducerEnhancer =
   createStore => (reducer, initialState, enhancer) => {
     const monitoredReducer = (state, action) => {
-      const start = performance.now()
+      // const start = performance.now()
       const newState = reducer(state, action)
-      const end = performance.now()
-      const diff = round(end - start)
+      // const end = performance.now()
+      // const diff = round(end - start)
 
-      console.log('reducer process time:', diff)
+      // console.log('reducer process time:', diff)
 
       return newState
     }
